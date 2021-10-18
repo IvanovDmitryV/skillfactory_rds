b5637cf8b87dab3ff399ad8629a3080277c50979
Dmitry Ivanov <manych@mail.ru>
2021-10-19 00:55:32 +0300

    ticket_flight added

--- module_4new/Project_draft.sql
+++ module_4new/Project_draft.sql
@@ -11,8 +11,56 @@ where
     fl.status not in ('Cancelled') 
 order by 
     fl.scheduled_departure
-
-
+	
+/************************************************************/
+select
+    tf.flight_id,
+    count (*) as all_pass_count,
+    sum((tf.fare_conditions = 'Business')::int) as Business_cnt,
+    sum((tf.fare_conditions = 'Economy')::int) as Economy_cnt,
+    sum(tf.amount) as all_pass_amount,
+    sum((tf.fare_conditions = 'Business')::int * tf.amount) as Business_amount,
+    sum((tf.fare_conditions = 'Economy')::int * tf.amount) as Economy_amount
+from 
+    dst_project.ticket_flights as tf
+group by 
+    tf.flight_id
+/**************************************************************************************************/
+select 
+    flt.flight_id,
+    count(*) as aircraft_capacity,
+    sum((st.fare_conditions = 'Business')::int) as Business_class,
+    sum((st.fare_conditions = 'Economy')::int) as Economy_class
+from    
+    (
+    select 
+        row_number() over (order by ararr.city,fl.scheduled_departure),
+        fl.flight_id,
+        ardep.city as departure_from,
+        ararr.city as arrival_to,
+        fl.aircraft_code,
+        fl.actual_departure,
+        fl.actual_arrival,
+        extract(epoch from(fl.actual_arrival - fl.actual_departure))/60 as duration, 
+        extract(epoch from
+            (lead(fl.scheduled_departure) over (partition by ararr.city order by fl.scheduled_departure) - fl.scheduled_departure))/3600 as to_next_flight ,
+        extract(epoch from
+            (fl.scheduled_departure - lag(fl.scheduled_departure) over (partition by ararr.city order by fl.scheduled_departure)))/3600 as after_prev_flight
+    
+    from 
+        dst_project.flights as fl
+            join dst_project.airports as ardep on fl.departure_airport = ardep.airport_code
+                join dst_project.airports as ararr on fl.arrival_airport = ararr.airport_code
+    where 
+        fl.departure_airport = 'AAQ' and 
+        (date_trunc('month', fl.scheduled_departure) in ('2017-01-01','2017-02-01', '2017-12-01')) and 
+        fl.status not in ('Cancelled')
+    ) as flt
+        join dst_project.seats as st on flt.aircraft_code = st.aircraft_code
+group by 
+        flt.flight_id     
+        
+        
 /**************************************************************************************************/
 select 
     *
@@ -88,3 +136,4 @@ where
     fl.status not in ('Cancelled') 
 order by 
     fl.scheduled_departure*/
+	
