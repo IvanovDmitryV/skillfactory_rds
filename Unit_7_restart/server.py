c620784eb7b6450144ac93792c92e1ab5a96e714
IvanovDmitryV <manych@mail.ru>
2023-03-05 09:22:23 +0300

    fin project start point

--- Unit_7_restart/server.py
+++ Unit_7_restart/server.py
@@ -8,6 +8,7 @@ with open('./models/model_task_7_6.pkl', 'rb') as pkl_file:
 
 app = Flask(__name__)
 
+
 @app.route('/hello')
 def hello_func():
     name = request.args.get('name')
@@ -17,6 +18,8 @@ def hello_func():
 # Напишите новую функцию index(), которая будет возвращать строку "Test message. The server is running".
 # Оберните эту функцию в декоратор app.route(), указав в качестве эндпоинта '/'. Данный эндпоинт будет
 # соответствовать обращению к сайту по дефолтному адресу: http://localhost:5000/.
+
+
 @app.route('/')
 def index():
     return "Test message. The server is running"
@@ -25,11 +28,15 @@ def index():
 # Напишите функцию current_time, которая будет возвращать словарь, где ключ — 'time',
 # а значение — текущее время.
 # Оберните эту функцию в декоратор app.route() с эндпоинтом /time.
+
+
 @app.route('/time')
 def current_time():
     now = datetime.datetime.now()
     return {'time': now}
 # ===
+
+
 @app.route('/add', methods=['POST'])
 def add():
     num = request.json.get('num')
@@ -46,19 +53,19 @@ def add():
 # Так как на вход модели необходимо подать numpy-массив определённой размерности, а не список, не забудьте
 # перевести результат в тип np.array() и скорректировать его под размер (1, 4) с помощью reshape()
 # Ответом на запрос должен быть JSON-формат {"prediction": *число - предсказание модели*}.
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     features = np.array(request.json)
     features = features.reshape(1, 4)
     prediction = model.predict(features)
-    return  jsonify({'prediction': prediction[0]})
-
-
-
+    return jsonify({'prediction': prediction[0]})
 
 # ======================================================================================
+
+
 if __name__ == '__main__':
 
     # app.run('localhost', 5000)
     app.run('127.0.0.1', 5000)
-
